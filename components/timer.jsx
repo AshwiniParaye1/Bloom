86f50055dae2dc129e019904a54ee7f354154096
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-09 20:34:12 +0530

    created pomodoro

--- components/timer.jsx
+++ components/timer.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { IoPlayOutline, IoPauseOutline, IoRestart } from "react-icons/io5";
+import { IoPlayOutline, IoPauseOutline } from "react-icons/io5";
 import { RiRestartLine } from "react-icons/ri";
 
 const Timer = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  };
+
+  // Handle timer countdown
+  useEffect(() => {
+    let timer;
+    if (isRunning && time > 0) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, time]);
+
   return (
     <>
       <div className="flex gap-2">
@@ -19,21 +46,19 @@ const Timer = () => {
       </div>
 
       <div>
-        <p>25 : 00</p>
+        <p>{formatTime(time)}</p>
       </div>
 
+      {/* start/pause, restart */}
       <div className="flex gap-2">
         <div className="flex gap-2">
-          <Button variant="outline">
-            <IoPlayOutline />
-          </Button>
-          <Button variant="outline" className="hidden">
-            <IoPauseOutline />
+          <Button variant="outline" onClick={() => setIsRunning(!isRunning)}>
+            {isRunning ? <IoPlayOutline /> : <IoPauseOutline />}
           </Button>
         </div>
 
         <div>
-          <Button variant="outline" className="">
+          <Button variant="outline" onClick={() => setTime(25 * 60)}>
             <RiRestartLine />
           </Button>
         </div>
