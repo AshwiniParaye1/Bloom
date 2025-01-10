7505e91b2575dbfcf678440d92da918750c99f22
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-10 16:31:13 +0530

    added images

--- components/navItems/BackgroundImages/backgroundImages.jsx
+++ components/navItems/BackgroundImages/backgroundImages.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
 import { IoImagesOutline } from "react-icons/io5";
 
 const BackgroundImages = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const images = [
+    "/bg6.jpeg",
+    "/bg7.jpeg",
+    "/blossom.jpeg",
+    "/cafeteria.jpeg",
+    "/dark-academia.jpeg",
+    "/dreamy-clouds.jpeg"
+  ];
+
+  const handleBgImage = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className="text-gray-200">
-      <IoImagesOutline size={25} />
-    </div>
+    <>
+      <div className="text-gray-200">
+        <button onClick={() => handleBgImage()}>
+          <IoImagesOutline size={25} />
+        </button>
+      </div>
+
+      {isVisible && (
+        <div className="absolute top-1/2 left-16 -translate-y-1/2 w-64 max-h-64 overflow-y-scroll bg-gradient-to-bl from-black to-transparent p-4 rounded-xl flex flex-col gap-4">
+          {images.map((src, index) => (
+            <Image
+              key={index}
+              src={src}
+              alt="scene"
+              width={400}
+              height={30}
+              className="w-full h-36 object-cover rounded-xl"
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
