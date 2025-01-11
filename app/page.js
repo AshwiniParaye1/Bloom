cffc6a05f1e84570e61093a6b7ab7d4cafb3c6be
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-11 12:26:34 +0530

    added clock

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavItems from "@/components/navItems/navitems";
 import Timer from "@/components/timer";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   const [backgroundImage, setBackgroundImage] = useState("/cafeteria.jpeg");
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
@@ -16,6 +26,10 @@ const Home = () => {
         }}
       >
         <Timer />
+
+        <div className="absolute top-8 right-8 -translate-y-1/2 ">
+          <p className=" text-gray-200 text-xl">{time.toLocaleTimeString()}</p>
+        </div>
 
         <div className="absolute top-1/2 left-0 -translate-y-1/2 ml-8">
           <NavItems onSelectImage={setBackgroundImage} />
