3ab0d9d35464467c878a9864327862e0b5589afc
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-11 12:04:02 +0530

    added more bg

--- app/constants/bgImages.js
+++ app/constants/bgImages.js
@@ -3,8 +3,11 @@ export const images = [
   "/bg2.jpeg",
   "/bg3.jpeg",
   "/bg4.jpeg",
+  "/vintage1.jpeg",
+  "/flower.jpeg",
   "/bg5.jpeg",
   "/bg6.jpeg",
+  "/bg7.jpeg",
   "/blossom.jpeg",
   "/cafeteria.jpeg",
   "/dark-academia.jpeg"
