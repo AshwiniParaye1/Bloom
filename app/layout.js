c3c56ca4f795fff53ddc17311686bb1c681f1a33
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-12 20:58:30 +0530

    added og data

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,10 +21,39 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:image" content="/lotus.png" />
-        <meta name="twitter:image" content="<generated>" />
-      </head>
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        {/* Open Graph (OG) Metadata for Social Media */}
+        <meta property="og:title" content="Bloom" />
+        <meta property="og:description" content="Bloom where you are planted" />
+        <meta
+          property="og:image"
+          content="https://www.bloomwithin.site/lotus.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.bloomwithin.site" />
+        <meta property="og:site_name" content="Bloom" />
+
+        {/* Twitter Card Metadata */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Bloom" />
+        <meta
+          name="twitter:description"
+          content="Bloom where you are planted"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.bloomwithin.site/lotus.png"
+        />
+      </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
