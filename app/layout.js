120304cdac21760bb0a1a29a0aa5ab29af8bbd28
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-12 20:45:53 +0530

    update meta img

--- app/layout.js
+++ app/layout.js
@@ -13,12 +13,16 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Bloom",
-  description: "Bloom where you are planted"
+  description: "Bloom where you are planted",
+  image: "/lotus.png"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="/lotus.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
