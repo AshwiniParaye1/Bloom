8c7c57a979dbd00945f77af8f16a48b808bfe30a
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-13 11:43:46 +0530

    removed some tracks

--- app/constants/bgTracks.js
+++ app/constants/bgTracks.js
@@ -8,10 +8,6 @@ export const tracks = [
     url: "https://soundcloud.com/lofi_study_christmas/sets/lofi-beats-chill-sad-study-sleep-relax-jazz-guitar-piano-instrumental-relaxing-music-rap-mix-lo-fi-soft-lofi-hip-hop-christmas-music-chritmas-songs?si=05cccd21adf046a5abdac299bfc5e420&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
   },
   {
-    name: "Sacred Strings",
-    url: "https://soundcloud.com/harpinder-bhachu/sets/indian-sitar-music?si=c5d95f5ec51347408ae059e8008d330c&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-  },
-  {
     name: "Tranquil Keys",
     url: "https://soundcloud.com/relaxing-music-production/sets/piano-covers?si=f2e66dd05bb141fe94eb19b3f896dd9d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
   },
@@ -22,10 +18,6 @@ export const tracks = [
   {
     name: "Blissful Flute",
     url: "https://soundcloud.com/brahmakumaris-bk/krishna-flute-music-happy-bliss-music-essence-of-life?si=575963fc874d42ad974c57a8eebbbeea&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-  },
-  {
-    name: "Echoes of Sitar",
-    url: "https://soundcloud.com/harpinder-bhachu/sets/indian-sitar-music?si=951b4d9b604b46ed90566b7aaf551a54&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
   },
   {
     name: "Zen Melodies",
@@ -46,10 +38,6 @@ export const tracks = [
   {
     name: "Soulful Sitar",
     url: "https://soundcloud.com/amanda-garcia-849358505/sets/sitar-music?si=399d4b83cc0a4ee2aa0c7126b238da17&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-  },
-  {
-    name: "Rasiya Rhythms",
-    url: "https://soundcloud.com/rishabsmusic/dhun-rasiya-at-chhandayan-live?si=c533ab2095d442b6aea6ba4a1742fef6&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
   },
   {
     name: "Ambient Serenity",
