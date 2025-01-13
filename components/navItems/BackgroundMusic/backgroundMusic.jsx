4d9e15f6cf751368d8c53693991e9c7d81dbdd4e
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-13 11:16:41 +0530

    added loader

--- components/navItems/BackgroundMusic/backgroundMusic.jsx
+++ components/navItems/BackgroundMusic/backgroundMusic.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { tracks } from "@/app/constants/bgTracks";
 import { Button } from "@/components/ui/button";
 import React, { useState, useRef, useEffect } from "react";
@@ -8,6 +6,7 @@ import { IoMusicalNotesOutline, IoClose } from "react-icons/io5";
 const BackgroundMusic = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [currentTrack, setCurrentTrack] = useState(tracks[0]); // Default track
+  const [isLoading, setIsLoading] = useState(false); // Loading state
   const iframeRef = useRef(null);
 
   const handleMusicTrack = () => {
@@ -16,16 +15,28 @@ const BackgroundMusic = () => {
 
   // Function to switch track
   const handleTrackSelection = (track) => {
+    setIsLoading(true); // Start loader
     setCurrentTrack(track);
     setIsVisible(false);
   };
 
   useEffect(() => {
-    // Ensure iframe reloads with the new track when changed
     if (iframeRef.current) {
       iframeRef.current.src = `https://w.soundcloud.com/player/?url=${encodeURIComponent(
         currentTrack
       )}&auto_play=true`;
+
+      // Listen for iframe 'load' event to stop the loader
+      const iframe = iframeRef.current;
+      const onLoad = () => {
+        setIsLoading(false); // Stop loader
+      };
+
+      iframe.addEventListener("load", onLoad);
+
+      return () => {
+        iframe.removeEventListener("load", onLoad);
+      };
     }
   }, [currentTrack]);
 
@@ -48,7 +59,7 @@ const BackgroundMusic = () => {
       ></iframe>
 
       {isVisible && (
-        <div className="absolute sm:top-1/2 left-16 sm:-translate-y-1/2 sm:w-64 sm:max-h-64 w-40 max-h-40  top-0 overflow-y-scroll bg-gradient-to-bl from-black to-transparent p-4 rounded-xl flex flex-col gap-4">
+        <div className="absolute sm:top-1/2 left-16 sm:-translate-y-1/2 sm:w-64 sm:max-h-64 w-40 max-h-40 top-0 overflow-y-scroll bg-gradient-to-bl from-black to-transparent p-4 rounded-xl flex flex-col gap-4">
           <button
             className="text-gray-200 self-end text-xl"
             onClick={() => setIsVisible(false)}
@@ -66,6 +77,12 @@ const BackgroundMusic = () => {
               {track.name}
             </Button>
           ))}
+        </div>
+      )}
+
+      {isLoading && (
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="loader"></div>
         </div>
       )}
     </>
